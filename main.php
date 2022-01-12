26ad3bffc7b4b3af8d3656423909112db8819890
Mohammad javad Hossein pour <mjavadhpour@gmail.com>
2022-01-12 18:05:55 +0330

    refactor: use general partial builder

--- main.php
+++ main.php
@@ -27,7 +27,11 @@
         ],
     ];
 
-    $get_position = fn($array) => $map(fn($item) => $item['position'], $array);
+    $get_partial = fn($index) => fn($array) => $map(fn($item) => $item[$index], $array);
+
+    $get_position = $get_partial('position');
+    $get_name = $get_partial('name');
 
     print_r( $get_position( EMPLOYEES ) );
+    print_r( $get_name( EMPLOYEES ) );
 ?>
