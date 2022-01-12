7e07ce7d7f4e79af43974acb07885ce9c7a4b717
Mohammad javad Hossein pour <mjavadhpour@gmail.com>
2022-01-12 19:13:34 +0330

    feat: value or array challenge

--- main.php
+++ main.php
@@ -43,4 +43,31 @@
 
     print_r($tally_votes(ALL_VOTES));
 
+    $second_arg_isnt_zero = fn(Closure $func) => function(...$args) use ($func) {
+        if ($args[1] === 0) {
+            echo "Second arg can not be zero!\n";
+            return null;
+        }
+        return $func(...$args);
+    };
+
+    $call_on_value_or_array = fn(Closure $func) => function(...$args) use ($func) {
+        if (is_array($args[0])) {
+            return array_map(fn($item) => $func($item), $args[0]);
+        }
+        return $func($args[0]);
+    };
+
+    $double = fn($x) => $x * 2;
+    $double_wrapped = $call_on_value_or_array($double);
+
+    $value = 4;
+    $doubled_value = $double_wrapped($value);
+    
+    $array = [1, 2, 3, 4];
+    $doubled_array = $double_wrapped($array);
+
+    echo $doubled_value. "\n";
+    print_r($doubled_array);
+
 ?>
